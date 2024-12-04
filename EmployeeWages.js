acb5c34a8a91d771609d7b705bc52c95ac594c64
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2024-12-04 11:40:46 +0530

    Akshat update EmployeeWages UC2

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -17,3 +17,35 @@ function calculateAttendance() {
 let ans = calculateAttendance()
 
 console.log(ans);
+
+2. //Ability to Calculate Daily Employee Wage based on part time or full time work
+
+function getRandomValue() {
+    const values = [0, 4, 8];
+    const randomIndex = Math.floor(Math.random() * values.length);
+    return values[randomIndex];
+}
+
+function employeeWage() {
+    const time = getRandomValue()
+    console.log("Time Worked: " + time + " Hours");
+    let salary = 0;
+
+    switch (time) {
+        case 4:
+            return 4*20
+            break;
+        case 8: 
+        return 8*20
+    
+        default:
+            break;
+    }
+
+    return salary;
+}
+
+console.log("Salary get: $" + employeeWage());
+    
+
+
